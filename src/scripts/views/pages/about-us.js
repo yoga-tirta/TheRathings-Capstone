449853177ca1cb7c1b32560a3c65b6a8871ad194
raehanrmdhn25 <raehan251101@gmail.com>
2023-05-31 00:28:06 +0700

    edit about us

--- src/scripts/views/pages/about-us.js
+++ src/scripts/views/pages/about-us.js
@@ -3,52 +3,46 @@ const AboutUs = {
       return `
       <section class="about-therathings">
       <div class="therathings-logo">
-        <img src="therathings.png" alt="logo therathings">
+        <img src="./favicon.png" alt="logo therathings">
       </div>
       <div class="therathings-description">
-        <h1>Tentang therathings</h1>
-        <p>therathings adalah sebuah website yang dibangun untuk memberikan informasi mengenai resep dan detail kandungan 
-        bahan dan produk makanan untuk membantu mengatur dan memenuhi kebutuhan nutrisi. Food Intake diharapkan mampu  
-        membantu pengguna merencanakan asupan makanan yang dibutuhkan.
+        <h1>Tentang TheRathings</h1>
+        <p>Anda saat ini sedang mengakses Website TheRathings. TheRathings dibangun dan dirancang oleh empat orang dengan visi untuk membantu mengelola permasalahan leftover food (makanan sisa). TheRathings juga memberikan rekomendasi tentang gaya hidup yang sehat dengan fokus pada pemilihan makanan dan minuman yang sehat.
         </p>
         </div>
     </section>
   
     <section class="about-team">
       <div class="team-description">
-        <h1>Team Kami</h1>
-        <p>Kami adalah sekelompok mahasiswa - mahasiswi Indonesia yang berfokus dalam dunia software developer 
-        khususnya dalam bidang Front-End dan Back End. Misi kami kali ini adalah untuk membantu sesama terutama 
-        masyarakat disekitar kami. Harapan utama dibuatnya therathings adalah agar dapat membantu masyarakat dalam 
-        memberikan referensi menu makanan kepada masyarakat yang tidak hanya memberikan resep, namun juga kandungan 
-        dan gizi dari menu makanan tersebut.
+        <h1>Team</h1>
+        <p>Team kami terdiri dari mahasiswa dan mahasiswi yang berfokus dalam dunia Teknologi Informasi, khususnya dalam bidang Front-End dan Back End Website Developer. Misi kami adalah untuk membantu sesama terutama masyarakat yang ada disekitar kami. Harapan kami dengan dibuatnya Website TheRathings adalah agar dapat membantu masyarakat dalam memberikan referensi tentang Gaya Hidup yang sehat dengan fokus pada pemilihan makanan dan minuman yang sehat untuk dikonsumsi.
      </p> 
       </div>
       <div class="team-profile">
         <div class="card-profile">
-          <img src="/user/yoga.png" alt="Seoarang mahasiswa">
+          <img src="./img/team/yoga-tirta-permana.png" alt="Seoarang mahasiswa">
           <h1>Yoga Tirta Permana</h1>
           <h2>Full-Stack</h2>
           <a href="https://www.linkedin.com/in/yoga-tirta-permana/"><button>Connect</button></a>
         </div>
   
         <div class="card-profile">
-          <img src="/user/raehan.png" alt="Seoarang mahasiswa">
+          <img src="./img/team/raehan-ramadhan.png" alt="Seoarang mahasiswa">
           <h1>Raehan Ramadhan</h1>
           <h2>Full-Stack</h2>
-          <a href="#"><button>Connect</button></a>
+          <a href="https://www.linkedin.com/in/raehan-ramadhan/"><button>Connect</button></a>
         </div>
         
         <div class="card-profile">
-          <img src="/user/putri.jpg" alt="Seoarang mahasiswa">
+          <img src="/user/putri.jpg" alt="Seoarang mahasiswi">
           <h1>Nur Rahma Utami Putri</h1>
           <h2>Full-Stack</h2>
           <a href="#"><button>Connect</button></a>
         </div>
   
         <div class="card-profile">
-          <img src="/user/ipeh.png" alt="Seoarang mahasiswa">
-          <h1>Hannifah Sri Rinjani</h1>
+          <img src="./img/team/haniffah-sri-rinjani.png" alt="Seoarang mahasiswi">
+          <h1>Haniffah Sri Rinjani</h1>
           <h2>Full-Stack</h2>
           <a href="#"><button>Connect</button></a>
         </div>
