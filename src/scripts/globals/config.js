43be7acac8e451d352c0188e6eca7cdfc56ee1ec
yoga-tirta <yogatirtapermana552@gmail.com>
2023-05-31 10:36:55 +0700

    edit footer and about

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,12 +1,11 @@
 const CONFIG = {
-    API_KEY: '',
-    BASE_URL: 'https://api.spoonacular.com/',
-    NUMBER_PAGE: 'number=24',
-    DATABASE_NAME: 'therathings-database',
-    DATABASE_VERSION: 1,
-    OBJECT_STORE_NAME: 'foods',
-    CACHE_NAME: new Date().toISOString(),
-  };
-  
-  export default CONFIG;
-  
+  API_KEY: '41e6b308cc534e4e80822fd363e1da96',
+  BASE_URL: 'https://api.spoonacular.com/',
+  NUMBER_PAGE: 'number=24',
+  DATABASE_NAME: 'therathings-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'foods',
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
--- src/scripts/views/pages/about.js
+++ src/scripts/views/pages/about.js
@@ -1,6 +1,6 @@
 const AboutUs = {
-    async render() {
-      return `
+  async render() {
+    return `
       <section class="about-therathings">
       <div class="therathings-logo">
         <img src="./favicon.png" alt="logo therathings">
@@ -15,42 +15,40 @@ const AboutUs = {
     <section class="about-team">
       <div class="team-description">
         <h1>Team</h1>
-        <p>Team kami terdiri dari mahasiswa dan mahasiswi yang berfokus dalam dunia Teknologi Informasi, khususnya dalam bidang Front-End dan Back End Website Developer. Misi kami adalah untuk membantu sesama terutama masyarakat yang ada disekitar kami. Harapan kami dengan dibuatnya Website TheRathings adalah agar dapat membantu masyarakat dalam memberikan referensi tentang Gaya Hidup yang sehat dengan fokus pada pemilihan makanan dan minuman yang sehat untuk dikonsumsi.
-     </p> 
+        <p>Team kami terdiri dari mahasiswa dan mahasiswi yang berfokus dalam dunia Teknologi Informasi, khususnya dalam bidang Front-End dan Back End Website Developer. Misi kami adalah untuk membantu sesama terutama masyarakat yang ada disekitar kami. Harapan kami dengan dibuatnya Website TheRathings adalah agar dapat membantu masyarakat dalam memberikan referensi tentang Gaya Hidup yang sehat dengan fokus pada pemilihan makanan dan minuman yang sehat untuk dikonsumsi.</p> 
       </div>
       <div class="team-profile">
         <div class="card-profile">
-          <img src="./img/team/yoga-tirta-permana.png" alt="Seoarang mahasiswa">
+          <img src="./img/team/yoga-tirta-permana.png" alt="Yoga Tirta">
           <h1>Yoga Tirta Permana</h1>
-          <h2>Full-Stack</h2>
-          <a href="https://www.linkedin.com/in/yoga-tirta-permana/"><button>Connect</button></a>
+          <h2>Full-Stack Web Developer</h2>
+          <a href="https://www.linkedin.com/in/yoga-tirta-permana/"><button>LinkedIn</button></a>
         </div>
   
         <div class="card-profile">
-          <img src="./img/team/raehan-ramadhan.png" alt="Seoarang mahasiswa">
+          <img src="./img/team/raehan-ramadhan.png" alt="Raehan">
           <h1>Raehan Ramadhan</h1>
-          <h2>Full-Stack</h2>
-          <a href="https://www.linkedin.com/in/raehan-ramadhan/"><button>Connect</button></a>
+          <h2>Full-Stack Web Developer</h2>
+          <a href="https://www.linkedin.com/in/raehan-ramadhan/"><button>LinkedIn</button></a>
         </div>
         
         <div class="card-profile">
-          <img src="/user/putri.jpg" alt="Seoarang mahasiswi">
+          <img src="./img/team/putri.png" alt="Utami Putri">
           <h1>Nur Rahma Utami Putri</h1>
-          <h2>Full-Stack</h2>
-          <a href="#"><button>Connect</button></a>
+          <h2>Full-Stack Web Developer</h2>
+          <a href="#"><button>LinkedIn</button></a>
         </div>
   
         <div class="card-profile">
-          <img src="./img/team/haniffah-sri-rinjani.png" alt="Seoarang mahasiswi">
+          <img src="./img/team/haniffah-sri-rinjani.png" alt="Haniffah">
           <h1>Haniffah Sri Rinjani</h1>
-          <h2>Full-Stack</h2>
-          <a href="#"><button>Connect</button></a>
+          <h2>Full-Stack Web Developer</h2>
+          <a href="#"><button>LinkedIn</button></a>
         </div>
       </div>
     </section>
           `;
-    },
-  };
-  
-  export default AboutUs;
-  
+  },
+};
+
+export default AboutUs;
