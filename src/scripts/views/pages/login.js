ca216aa7f3a0246c3419f79908cb8ae44bcd859e
yoga-tirta <yogatirtapermana552@gmail.com>
2023-06-04 21:35:15 +0700

    login & register

--- src/scripts/views/pages/login.js
+++ src/scripts/views/pages/login.js
@@ -1,10 +1,38 @@
 const Login = {
   async render() {
     return `
-      <section class="hero">
-        <h1>Log In</h1>
-        <p>Masuk Akun</p>
-      </section>
+    <div class="login">
+    <div class="login__content">
+        <div class="login__img">
+            <img src="./img/img-login.svg" alt="Login Image">
+        </div>
+
+        <div class="login__forms">
+            <form action="" class="login__registre" id="login-in">
+                <h1 class="login__title">Sign In</h1>
+
+                <div class="login__box">
+                    <i class='fa fa-user login__icon'></i>
+                    <input type="text" placeholder="Username" class="login__input">
+                </div>
+
+                <div class="login__box">
+                    <i class='fa fa-lock login__icon'></i>
+                    <input type="password" placeholder="Password" class="login__input">
+                </div>
+
+                <a href="#" class="login__forgot">Forgot password?</a>
+
+                <a href="#" class="login__button">Sign In</a>
+
+                <div>
+                    <span class="login__account">Don't have an Account ?</span>
+                    <a href="#/register" class="login__signin" id="sign-up">Sign Up</a>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
     `;
   },
 };
--- src/scripts/views/pages/register.js
+++ src/scripts/views/pages/register.js
@@ -1,10 +1,47 @@
 const Register = {
   async render() {
     return `
-      <section class="hero">
-        <h1>Register</h1>
-        <p>Daftar Akun</p>
-      </section>
+    <div class="login">
+    <div class="login__content">
+        <div class="login__img">
+            <img class="img-login" src="./img/img-login.svg" alt="Login Image">
+        </div>
+
+        <div class="login__forms">
+            <form action="" class="login__create" id="login-up">
+                <h1 class="login__title">Create Account</h1>
+
+                <div class="login__box">
+                    <i class='fa fa-user login__icon'></i>
+                    <input type="text" placeholder="Username" class="login__input">
+                </div>
+
+                <div class="login__box">
+                    <i class='fa fa-at login__icon'></i>
+                    <input type="text" placeholder="Email" class="login__input">
+                </div>
+
+                <div class="login__box">
+                    <i class='fa fa-lock login__icon'></i>
+                    <input type="password" placeholder="Password" class="login__input">
+                </div>
+
+                <a href="#" class="login__button">Sign Up</a>
+
+                <div>
+                    <span class="login__account">Already have an Account ?</span>
+                    <a href="#/login" class="login__signup" id="sign-in">Sign In</a>
+                </div>
+
+                <div class="login__social">
+                    <a href="#" class="login__social-icon"><i class='fab fa-facebook' ></i></a>
+                    <a href="#" class="login__social-icon"><i class='fab fa-instagram' ></i></a>
+                    <a href="#" class="login__social-icon"><i class='fab fa-google' ></i></a>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
     `;
   },
 };
