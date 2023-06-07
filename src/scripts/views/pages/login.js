61142a88ac1b09ea5ee586248786738d0a5c6f62
raehanrmdhn25 <raehan251101@gmail.com>
2023-06-07 23:43:37 +0700

    login and register

--- src/scripts/views/pages/login.js
+++ src/scripts/views/pages/login.js
@@ -2,31 +2,31 @@ const Login = {
   async render() {
     return `
       <div class="login">
-        <div class="login__content">
-          <div class="login__img">
+        <div class="login-content">
+          <div class="login-img">
             <img src="./img/img-login.svg" alt="Login Image">
           </div>
 
-          <div class="login__forms">
-            <form action="" class="login__registre" id="login-in">
-              <h1 class="login__title">Sign In</h1>
+          <div class="login-forms">
+            <form action="" class="login-register" id="login-in">
+              <h1 class="login-title">Sign In</h1>
 
-              <div class="login__box">
-                <i class='fa fa-user login__icon'></i>
-                <input type="text" placeholder="Username" class="login__input">
+              <div class="login-box">
+                <i class='fa fa-user login-icon'></i>
+                <input type="text" placeholder="Username" class="login-input">
               </div>
 
-              <div class="login__box">
-                <i class='fa fa-lock login__icon'></i>
-                <input type="password" placeholder="Password" class="login__input">
+              <div class="login-box">
+                <i class='fa fa-lock login-icon'></i>
+                <input type="password" placeholder="Password" class="login-input">
               </div>
 
-              <a href="#" class="login__forgot">Forgot password?</a>
-              <a href="#" class="login__button">Sign In</a>
+              <a href="#" class="login-forgot">Forgot password?</a>
+              <a href="#" class="login-button">Sign In</a>
 
               <div>
-                <span class="login__account">Don't have an Account?</span>
-                <a href="#/register" class="login__signin" id="sign-up">Register</a>
+                <span class="login-account">Don't have an Account?</span>
+                <a href="#/register" class="login-sign-in" id="sign-up">Register</a>
               </div>
             </form>
           </div>
--- src/scripts/views/pages/register.js
+++ src/scripts/views/pages/register.js
@@ -2,41 +2,41 @@ const Register = {
   async render() {
     return `
       <div class="login">
-        <div class="login__content">
-          <div class="login__img">
+        <div class="login-content">
+          <div class="login-img">
             <img class="img-login" src="./img/img-login.svg" alt="Login Image">
           </div>
 
-          <div class="login__forms">
-            <form action="" class="login__create" id="login-up">
-              <h1 class="login__title">Register</h1>
+          <div class="login-forms">
+            <form action="" class="login-create" id="login-up">
+              <h1 class="login-title">Register</h1>
 
-              <div class="login__box">
-                <i class='fa fa-user login__icon'></i>
-                <input type="text" placeholder="Username" class="login__input">
+              <div class="login-box">
+                <i class='fa fa-user login-icon'></i>
+                <input type="text" placeholder="Username" class="login-input">
               </div>
 
-              <div class="login__box">
-                <i class='fa fa-at login__icon'></i>
-                <input type="text" placeholder="Email" class="login__input">
+              <div class="login-box">
+                <i class='fa fa-at login-icon'></i>
+                <input type="text" placeholder="Email" class="login-input">
               </div>
 
-              <div class="login__box">
-                <i class='fa fa-lock login__icon'></i>
-                <input type="password" placeholder="Password" class="login__input">
+              <div class="login-box">
+                <i class='fa fa-lock login-icon'></i>
+                <input type="password" placeholder="Password" class="login-input">
               </div>
 
-              <a href="#" class="login__button">Register</a>
+              <a href="#" class="login-button">Register</a>
 
               <div>
-                <span class="login__account">Already have an Account?</span>
-                <a href="#/login" class="login__signup" id="sign-in">Sign In</a>
+                <span class="login-account">Already have an Account?</span>
+                <a href="#/login" class="login-sign-up" id="sign-in">Sign In</a>
               </div>
 
-              <div class="login__social">
-                <a href="#" class="login__social-icon"><i class='fab fa-facebook' ></i></a>
-                <a href="#" class="login__social-icon"><i class='fab fa-instagram' ></i></a>
-                <a href="#" class="login__social-icon"><i class='fab fa-google' ></i></a>
+              <div class="login-social-media">
+                <a href="#" class="login-social-media-icon"><i class='fab fa-facebook' ></i></a>
+                <a href="#" class="login-social-media-icon"><i class='fab fa-instagram' ></i></a>
+                <a href="#" class="login-social-media-icon"><i class='fab fa-google' ></i></a>
               </div>
             </form>
           </div>
