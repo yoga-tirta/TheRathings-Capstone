f8f839b829626ac25a9cf3360add554aa36e74f4
yoga-tirta <yogatirtapermana552@gmail.com>
2023-06-17 11:41:50 +0700

    edit api key and build

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // API_KEY: 'apiKey=c8a6bc0617174df1a11e2f3fdf97c101', // first api
-  API_KEY: 'apiKey=0323c83ff30044edbb34441372584009', // second api
-  // API_KEY: 'apiKey=c2225c35118748efbf02a8f5f8479062', // third api
+  // API_KEY: 'apiKey=0323c83ff30044edbb34441372584009', // second api
+  API_KEY: 'apiKey=c2225c35118748efbf02a8f5f8479062', // third api
 
   BASE_URL: 'https://api.spoonacular.com/',
   NUMBER_PAGE: 'number=24',
